350b32ee0c8a1073a3968e671fd53e7db8a6ffa5
DeSi <163108056+desilvavelasquez123@users.noreply.github.com>
2026-09-09 22:01:42 +0200

    Set the base for the RemoveFromArray exerise
    The basic (more than 50% of the exercise) established
    Practicing Commiting a bit

--- foundations/loops_and_arrays/01_repeatString/repeatString.js
+++ foundations/loops_and_arrays/01_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
+const repeatString = function (str, num) {
+    if (num < 0) { return "ERROR" } else {
+        let counter = 0;
+        let repeatedString = "";
+        while (counter < num) {
+            repeatedString += str;
+            counter++;
+        }
+        return repeatedString;
+    }
+}
 
-};
-
-// Do not edit below this line
 module.exports = repeatString;
