1a6a085851605d3ad9c21c7cc8d413db7eee330c
DeSi <163108056+desilvavelasquez123@users.noreply.github.com>
2026-09-11 00:16:04 +0200

    Exercise done!

--- foundations/loops_and_arrays/03_removeFromArray/removeFromArray.js
+++ foundations/loops_and_arrays/03_removeFromArray/removeFromArray.js
@@ -1,12 +1,12 @@
-const removeFromArray = function (array, number,...args) {
-  array.splice(number - 1, number / number);
-  for (const element in array) {
-    console.log(typeof element);
-    (element == number) ? array.splice(element - 1, 1) : element;
+const removeFromArray = function (array, ...args) {
+  (array.includes(args)) ? array = [] : array;
+  for (const element of array) {
+    for (const argument of args) {
+    (array.includes(argument)) ? array.splice(array.indexOf(argument), 1) : element;
+    }
   }
-  if (array.args != []) {array.splice(args - 1, args / args)};
-return array;
+  return array;
+};
 
-  };
 // Do not edit below this line
 module.exports = removeFromArray;
