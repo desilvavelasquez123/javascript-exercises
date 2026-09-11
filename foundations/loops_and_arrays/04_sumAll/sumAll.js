5f6ddd6eab31f4ffa54dff08e4da972669e55f19
DeSi <163108056+desilvavelasquez123@users.noreply.github.com>
2026-09-11 22:10:17 +0200

    Exercise base

--- foundations/loops_and_arrays/04_sumAll/sumAll.js
+++ foundations/loops_and_arrays/04_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    let sum = 0
+    if ( a > b) {
+        sum = 0;
+        for (let i = b; i <= a; i++) {
+            sum += i;
+        }
+        return sum;
+    } else {
+        sum = 0;
+        for (let i = a; i <= b; i++) {
+            sum += i;
+        }
+        return sum;
+    }
 };
 
 // Do not edit below this line
