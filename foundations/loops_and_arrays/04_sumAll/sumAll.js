c2f24c9101becabdad6fc8761894e4ec5afdfef4
DeSi <163108056+desilvavelasquez123@users.noreply.github.com>
2026-09-12 00:06:31 +0200

    Exercise done!

--- foundations/loops_and_arrays/04_sumAll/sumAll.js
+++ foundations/loops_and_arrays/04_sumAll/sumAll.js
@@ -1,17 +1,20 @@
-const sumAll = function(a, b) {
-    let sum = 0
-    if ( a > b) {
-        sum = 0;
-        for (let i = b; i <= a; i++) {
-            sum += i;
+const sumAll = function (a, b) {
+    if (a < 0 || b < 0 || !Number.isInteger(a) || !Number.isInteger(b) || a instanceof String || b instanceof String || typeof a !== "number" || typeof b !== "number" ) { return "ERROR" }
+    else {
+        let sum = 0
+        if (a > b) {
+            sum = 0;
+            for (let i = b; i <= a; i++) {
+                sum += i;
+            }
+            return sum;
+        } else {
+            sum = 0;
+            for (let i = a; i <= b; i++) {
+                sum += i;
+            }
+            return sum;
         }
-        return sum;
-    } else {
-        sum = 0;
-        for (let i = a; i <= b; i++) {
-            sum += i;
-        }
-        return sum;
     }
 };
 
